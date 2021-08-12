88e4ef750da9944b6817d5d39a5b133de9101314
Tianpeng Wang <tpwang@alauda.io>
2021-08-13 00:09:55 +0800

    chore: Adjust build script

--- ci/build.sh
+++ ci/build.sh
@@ -2,18 +2,18 @@
 
 set -eux
 
-# 配置 Rustup Mirror (Bytedance)
-export RUSTUP_DIST_SERVER="https://rsproxy.cn"
-export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
+# 配置 Rustup Mirror
+export RUSTUP_DIST_SERVER="https://mirrors.ustc.edu.cn/rust-static"
+export RUSTUP_UPDATE_ROOT="https://mirrors.ustc.edu.cn/rust-static/rustup"
 
-# 配置 creates.io Mirror (Bytedance)
-mkdir -p $HOME/.cargo
-cat <<EOF | tee $HOME/.cargo/config
+# 配置 creates.io Mirror
+mkdir -p ~/.cargo
+cat <<EOF | tee ~/.cargo/config
 [source.crates-io]
-replace-with = 'rsproxy'
+replace-with = 'ustc'
 
-[source.rsproxy]
-registry = "https://rsproxy.cn/crates.io-index"
+[source.ustc]
+registry = "https://mirrors.ustc.edu.cn/crates.io-index"
 EOF
 
 make build
