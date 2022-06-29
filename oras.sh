e8c9a6e16c0e00fc2f3cdf606e060ae9a8fcd140
Tianpeng Wang <tpwang@alauda.io>
2022-06-29 22:42:11 +0800

    comment oras.sh
    Signed-off-by: Tianpeng Wang <tpwang@alauda.io>

--- oras.sh
+++ oras.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-oras push \
-	build-harbor.alauda.cn/asm/wasm-add-header-rs-demo:v0-oras \
-	--manifest-config runtime-config.json:application/vnd.module.wasm.config.v1+json \
-	filter.wasm:application/vnd.module.wasm.content.layer.v1+wasm
-
-
+#oras push \
+#	build-harbor.alauda.cn/asm/wasm-limit-size-demo:v0-oras \
+#	--manifest-config runtime-config.json:application/vnd.module.wasm.config.v1+json \
+#	filter.wasm:application/vnd.module.wasm.content.layer.v1+wasm
